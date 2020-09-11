6df096c45f042ad4e4d3a4e935db8770b30244f5
TCH <chong.how97@live.com>
2020-09-11 21:47:21 +0800

    add Calendar

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import axios from "axios";
-
+Vue.prototype.$http = Axios;
 Vue.config.productionTip = true;
 Vue.prototype.$http = axios;
 new Vue({
